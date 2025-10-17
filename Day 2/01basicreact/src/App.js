cd40a5981ca1ef4755ebfef2402b467aab60c0b9
Jagrata Deb <jagratadeb2004@gmail.com>
2025-10-17 22:02:21 +0530

    React

---
+++ Day 2/01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Hello | agrata Deb</h1>
+  );
+}
+
+export default App;
---
+++ Day 2/01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Chai aur React with Vite</h1>
+      <h2>Jagrata Deb</h2>
+    </>
+  )
+}
+
+export default App
